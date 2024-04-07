be4019adc5e3a146ead3b521e7188f3485a19c77
florentino553 <149762922+florentino553@users.noreply.github.com>
2024-04-07 23:26:01 +0300

    concatenate_strings.sh

---
+++ concatenate_strings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# First string
+first_string="Hello"
+
+# Second string
+second_string="World"
+
+# Concatenating two strings
+connected_string="$first_string $second_string"
+
+# Outputting the result
+echo "$connected_string"
